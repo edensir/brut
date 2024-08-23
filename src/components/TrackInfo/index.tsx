1e47b315a8931726d8029e9d82f03456521d90e1
Eden Siregar <eden.siregar@alliander.com>
2024-08-23 10:45:59 +0200

    Pulling track name from our Spotify API data

--- src/components/TrackInfo/index.tsx
+++ src/components/TrackInfo/index.tsx
@@ -1,5 +1,14 @@
-const TrackInfo = () => {
-  return <h1>Hello authorized user</h1>;
+import { ITrack } from "../../types";
+import { H1 } from "./styles";
+
+interface IProps {
+  track: ITrack | null;
+}
+
+const TrackInfo: React.FC<IProps> = ({ track }) => {
+  if (!track || track.id === "") return null;
+
+  return <H1>{track?.name}</H1>;
 };
 
 export default TrackInfo;
---
+++ src/components/TrackInfo/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  color: black;
+  font-weight: bold;
+  font-size: 10vh;
+  padding-left: 0.5em;
+  max-width: 50vw;
+`;
